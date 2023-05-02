8ae0bdcd9dcb2f49d62eeeb4f1351aeb18289613
lalesca_moraes <lalesca.silva@zup.com.br>
2023-05-02 18:07:44 -0300

    atualizacao2

--- src/ExercicioQuatro.java
+++ src/ExercicioQuatro.java
@@ -1,18 +1,30 @@
 import java.util.Scanner;
 public class ExercicioQuatro {
-    Scanner sc = new Scanner(System.in);
-    int MenorValor = 0;
-    int VetorNumeros[] = new int [10];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] numeros = new int[6];
+        int menor;
 
-    for(int i = 0; i < VetorNumeros.length; i++) {
+        System.out.println("MENOR NUMERO");
 
-        if( VetorNumeros[i] < MenorValor){
-
-            MenorValor = VetorNumeros[i];
-
+        for (int i = 0; i < 6; i++) {
+            System.out.println("Digite os numeros: ");
+            numeros[i] = sc.nextInt();
         }
+        menor = numeros[0];
+
+        for (int i = 0; i < numeros.length; i++) {
+
+            if (menor > numeros[i]) {
+
+                menor = numeros[i];
+            }
+        }
+        System.out.println("O menor numero e: " + menor);
     }
 }
+
+
 
 
 
